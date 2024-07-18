17901ad489c2fc32ffc363662330545892a3bdd8
fiflak11 <273060@student.pwr.edu.pl>
2024-07-18 05:28:34 +0200

    glut setup
    add rendering
    camera setup

--- include/Snake.h
+++ include/Snake.h
@@ -1,13 +1,22 @@
 #ifndef SNAKE3D_SNAKE_H
 #define SNAKE3D_SNAKE_H
-
+#include <GL/glut.h>
 
 class Snake {
+    const static short FPS = 10;
+    const static short WIDTH = 800, HEIGHT = 600;
+    //camera setup
+    static GLdouble eyeX, eyeY, eyeZ;           //camera position
+    static GLdouble centerX, centerY, centerZ;  //the direction in which the camera is looking
+    static GLdouble upX, upY, upZ;              //upVector - camera orientation
+    //callbacks for glut
     static void display();
     static void reshape(int w,int h);
     static void keyboard(unsigned char key, int x, int y);
     static void specialKeys(int key, int x, int y);
+    static void timer(int);
 public:
+    //Snake();
     void startRendering(int argc, char * argv[]);
 };
 
--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,26 +1,44 @@
 #include "../include/Snake.h"
-
 #include <GL/glut.h>
-#include <memory>
-
+#include <iostream>
 
 void Snake::startRendering(int argc, char **argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(WIDTH, HEIGHT);
     glutCreateWindow("SNAKE 3D");
     glutDisplayFunc(&Snake::display);
     glutReshapeFunc(&Snake::reshape);
     glutKeyboardFunc(&Snake::keyboard);
     glutSpecialFunc(&Snake::specialKeys);
+    glutTimerFunc(0,&Snake::timer,0);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
     glutMainLoop();
 }
 void Snake::display(){
-
+    glClearColor( 0.0, 0.0, 0.0, 1.0 );
+    glClear( GL_COLOR_BUFFER_BIT );
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+    gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+    glColor3f( 1.0, 1.0, 1.0 );
+    //start rendering blocks
+    const int N=10,K=10;
+    for(int i{0}; i<N; i++){
+        for(int j{0}; j<K; j++){
+            glTranslatef( 1, 0.0, 0.0 );
+            glutWireCube( 1.0 );
+        }
+        glTranslatef( -K, 0.0, -1.0 );
+    }
+    glFlush();
+    glutSwapBuffers();
 }
 void Snake::reshape(int w,int h){
-
+    glViewport( 0, 0, WIDTH, HEIGHT );
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glFrustum( - 2.0, 2.0, - 2.0, 2.0, 1.0, 100.0 );
 }
 void Snake::keyboard(unsigned char key, int x, int y){
 
@@ -28,3 +46,19 @@ void Snake::keyboard(unsigned char key, int x, int y){
 void Snake::specialKeys(int key, int x, int y){
 
 }
+void Snake::timer(int){
+    glutPostRedisplay();
+    glutTimerFunc(1000/FPS,timer,0);
+}
+
+GLdouble Snake::eyeX = 5.7;
+GLdouble Snake::eyeY = 9.4;
+GLdouble Snake::eyeZ = 0.6;
+
+GLdouble Snake::centerX = 6;
+GLdouble Snake::centerY = -55;
+GLdouble Snake::centerZ = - 100;
+
+GLdouble Snake::upX = 0;
+GLdouble Snake::upY = 1;
+GLdouble Snake::upZ = 0;
